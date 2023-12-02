94cc40b9d4ededf5da20e8a2f5a7f478589e0782
Lorenzo Palaia <lorenzopalaia53@gmail.com>
2023-12-02 21:32:59 +0100

    nice secondary pages template

--- src/components/WorkExperience.vue
+++ src/components/WorkExperience.vue
@@ -1,41 +1,43 @@
 <template>
   <div class="container mx-auto my-auto">
-    <article class="prose">
-      <router-link to="/" class="no-underline">
-        <h4 class="mt-16 text-primary">
-          <font-awesome-icon icon="fa-solid fa-arrow-left" class="mr-2" />
-          Lorenzo Palaia
-        </h4>
-      </router-link>
-      <h1 class="mb-16">Work Experiences</h1>
+    <div class="flex flex-col mx-8">
+      <article class="prose">
+        <router-link to="/" class="no-underline">
+          <h4 class="mt-16 text-primary">
+            <font-awesome-icon icon="fa-solid fa-arrow-left" class="mr-2" />
+            Lorenzo Palaia
+          </h4>
+        </router-link>
+        <h1 class="mb-16">Work Experiences</h1>
+      </article>
       <div class="overflow-x-auto">
         <table class="table">
           <thead>
             <tr>
-              <th>Period</th>
-              <th>Company</th>
-              <th>Role</th>
-              <th>Location</th>
-              <th>Technologies</th>
-              <th>Link</th>
+              <th class="table-cell">Period</th>
+              <th class="table-cell">Company</th>
+              <th class="table-cell">Role</th>
+              <th class="hidden md:table-cell">Location</th>
+              <th class="hidden md:table-cell">Technologies</th>
+              <th class="hidden md:table-cell">Link</th>
             </tr>
           </thead>
           <tbody>
             <tr v-for="experience in workExperience" :key="experience">
-              <td>{{ experience.date }}</td>
-              <td>{{ experience.company }}</td>
-              <td>{{ experience.role }}</td>
-              <td>{{ experience.location }}</td>
-              <td>
+              <td class="table-cell">{{ experience.date }}</td>
+              <td class="table-cell">{{ experience.company }}</td>
+              <td class="table-cell">{{ experience.role }}</td>
+              <td class="hidden md:table-cell">{{ experience.location }}</td>
+              <td class="hidden md:table-cell">
                 <div
                   v-for="badge in experience.badges"
                   :key="badge"
-                  class="badge badge-lg badge-secondary mr-2 mb-8"
+                  class="badge badge-lg badge-secondary mr-2 mb-2"
                 >
                   <p class="text-primary">{{ badge }}</p>
                 </div>
               </td>
-              <td>
+              <td class="hidden md:table-cell">
                 <a :href="experience.link" target="_blank">
                   {{ experience.link }}
                 </a>
@@ -44,7 +46,7 @@
           </tbody>
         </table>
       </div>
-    </article>
+    </div>
   </div>
 </template>
 
