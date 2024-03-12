83325c523523875bf9a1df35aa6e2b6d9467755a
Lorenzo Palaia <lorenzopalaia53@gmail.com>
2024-03-12 23:19:56 +0100

    hash mode

--- src/router/index.js
+++ src/router/index.js
@@ -1,39 +1,39 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import WorkExperienceView from '../views/WorkExperienceView.vue'
-import ProjectsView from '../views/ProjectsView.vue'
-import EducationView from '../views/EducationView.vue'
-import ExtraActivitiesView from '../views/ExtraActivitiesView.vue'
+import { createRouter, createWebHashHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import WorkExperienceView from '../views/WorkExperienceView.vue';
+import ProjectsView from '../views/ProjectsView.vue';
+import EducationView from '../views/EducationView.vue';
+import ExtraActivitiesView from '../views/ExtraActivitiesView.vue';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
     },
     {
       path: '/work-experience',
       name: 'work-experience',
-      component: WorkExperienceView
+      component: WorkExperienceView,
     },
     {
       path: '/projects',
       name: 'projects',
-      component: ProjectsView
+      component: ProjectsView,
     },
     {
       path: '/education',
       name: 'education',
-      component: EducationView
+      component: EducationView,
     },
     {
       path: '/extra-activities',
       name: 'extra-activities',
-      component: ExtraActivitiesView
-    }
-  ]
-})
+      component: ExtraActivitiesView,
+    },
+  ],
+});
 
-export default router
+export default router;
