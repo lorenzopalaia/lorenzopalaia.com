bcfbfd1c84ca087357a3dcdca9a1eadd81cbf665
Lorenzo Palaia <lorenzopalaia53@gmail.com>
2024-04-16 21:44:14 +0200

    removed octokit test

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,10 +1,5 @@
-//import { Octokit } from "octokit";
-
 const CACHE_KEY = "projectsCache";
 const CACHE_EXPIRATION_TIME = 60 * 60 * 1000; // 60 minutes
-
-//const GITHUB_TOKEN = import.meta.env.VITE_GITHUB_TOKEN;
-//const octokit = new Octokit({ auth: GITHUB_TOKEN });
 
 async function fetchReposWithLanguages(username) {
   const response = await fetch(
@@ -19,29 +14,6 @@ async function fetchReposWithLanguages(username) {
   }
   return data;
 }
-
-/*
-async function fetchReposWithLanguages(username) {
-  try {
-    const { data: repos } = await octokit.rest.repos.listForUser({
-      username: username,
-    });
-    const promises = repos.map(async (repo) => {
-      const { data: languages } = await octokit.rest.repos.listLanguages({
-        owner: username,
-        repo: repo.name,
-      });
-      const repo_preview = `https://raw.githubusercontent.com/${username}/${repo.name}/main/repo_assets/preview.png`;
-      return { ...repo, languages: Object.keys(languages), repo_preview };
-    });
-    const data = await Promise.all(promises);
-    return data;
-  } catch (error) {
-    console.error("Error fetching repos:", error);
-    throw error;
-  }
-}
-*/
 
 async function getCachedProjects() {
   let cachedData = localStorage.getItem(CACHE_KEY);
