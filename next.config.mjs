6f1bd1e66bdce27e22ec9ab8b905f6a39a117110
lorenzopalaia <lorenzopalaia53@gmail.com>
2024-06-20 18:03:44 +0200

    SEO: HSTS headers

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,19 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=31536000; includeSubDomains; preload",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
