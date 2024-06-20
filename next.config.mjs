7fae76da2bdfb476ddbace22c3cadfe8a352a8db
lorenzopalaia <lorenzopalaia53@gmail.com>
2024-06-20 18:06:38 +0200

    SEO: content sniffing protection

--- next.config.mjs
+++ next.config.mjs
@@ -17,6 +17,10 @@ const nextConfig = {
             key: "Strict-Transport-Security",
             value: "max-age=31536000; includeSubDomains; preload",
           },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
         ],
       },
     ];
