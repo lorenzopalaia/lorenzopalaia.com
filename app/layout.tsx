27e4565cbf8b945c0315fed9cb597ee31631939d
lorenzopalaia <lorenzopalaia53@gmail.com>
2024-09-30 12:21:27 +0200

    DataFast Test

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { getSEOTags } from "@/libs/seo";
 import config from "@/config";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="scroll-smooth bg-slate-900">
+      <Head>
+        <script
+          defer
+          data-website-id="66fa7aac39ae75c2af18f9ce"
+          data-domain="lorenzopalaia.it"
+          src="https://datafa.st/js/script.js"
+        ></script>
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
