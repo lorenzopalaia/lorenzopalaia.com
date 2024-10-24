292363a1c4d7428aa559fb3f45b95edaa92766a0
lorenzopalaia <lorenzopalaia53@gmail.com>
2024-10-24 18:44:59 +0200

    Skills and Languages

--- app/page.tsx
+++ app/page.tsx
@@ -4,19 +4,25 @@ import Experience from "@/components/Home/Experience";
 // import Projects from "@/components/Home/Projects";
 import Education from "@/components/Home/Education";
 import ExtraActivities from "@/components/Home/ExtraActivities";
+import SkillsAndLangs from "@/components/Home/SkillsAndLangs";
+// import Footer from "@/components/Home/Footer";
 
 export default function Home() {
   return (
     <div className="max-w-screen-xl min-h-screen px-6 py-12 mx-auto font-sans md:px-12 md:py-20 lg:px-24 lg:py-0">
       <div className="lg:flex lg:gap-4 lg:justify-between">
         <Header />
-        <main className="pt-24 lg:w-1/2 lg:py-24">
-          <About />
-          <Experience />
-          {/* <Projects /> */}
-          <Education />
-          <ExtraActivities />
-        </main>
+        <div className="pt-24 lg:w-1/2 lg:py-24">
+          <main>
+            <About />
+            <Experience />
+            {/* <Projects /> */}
+            <Education />
+            <ExtraActivities />
+            <SkillsAndLangs />
+          </main>
+          {/* <Footer /> */}
+        </div>
       </div>
     </div>
   );
---
+++ components/Home/SkillsAndLangs.tsx
@@ -0,0 +1,46 @@
+import config from "@/config";
+import SectionTitle from "@/components/SectionTitle";
+import Progress from "@/components/Progress";
+
+export default function SkillsAndLangs() {
+  return (
+    <section
+      id="skillsAndLangs"
+      className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24"
+      aria-label="Skills & Languages"
+    >
+      <SectionTitle>Skills & Languages</SectionTitle>
+      <div className="mb-20">
+        {config.skills.map((skill, index) => (
+          <div key={skill.name} className="flex flex-col mb-9 lg:flex-row">
+            <div className="lg:w-1/4">
+              <p className="mt-2 mb-0 text-xs font-semibold tracking-wide uppercase text-slate-500">
+                {skill.name}
+              </p>
+            </div>
+            <div className="lg:w-3/4">
+              <Progress value={skill.value} index={index} />
+            </div>
+          </div>
+        ))}
+      </div>
+      <div>
+        {config.languages.map((language, index) => (
+          <div key={language.name} className="flex flex-col mb-9 lg:flex-row">
+            <div className="lg:w-1/4">
+              <p className="mt-2 mb-0 text-xs font-semibold tracking-wide uppercase text-slate-500">
+                {language.name}
+              </p>
+            </div>
+            <div className="lg:w-3/4">
+              <Progress
+                value={language.value}
+                index={index + config.skills.length}
+              />
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
