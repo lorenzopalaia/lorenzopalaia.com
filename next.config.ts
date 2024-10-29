931539cd6e561cc496f6172c42cd96589fd66a07
lorenzopalaia <lorenzopalaia53@gmail.com>
2024-10-29 14:23:45 +0100

    v2 init

--- next.config.ts
+++ next.config.ts
@@ -1,39 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "raw.githubusercontent.com",
-      },
-    ],
-  },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Strict-Transport-Security",
-            value: "max-age=31536000; includeSubDomains; preload",
-          },
-          {
-            key: "X-Content-Type-Options",
-            value: "nosniff",
-          },
-          {
-            key: "X-Frame-Options",
-            value: "DENY",
-          },
-          {
-            key: "X-XSS-Protection",
-            value: "1; mode=block",
-          },
-        ],
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
--- utils/formatLink.ts
+++
@@ -1,7 +0,0 @@
-export default function formatLink(link: string) {
-  return link
-    .replace("https://", "")
-    .replace("http://", "")
-    .replace("www.", "")
-    .split("/")[0];
-}
