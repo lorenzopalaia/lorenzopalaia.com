0ceb5a072aa5b0b843bc8d7c5c133ef075eb26ee
lorenzopalaia <lorenzopalaia53@gmail.com>
2024-11-13 17:04:23 +0100

    Minor

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,35 +3,27 @@ import Link from "next/link";
 import ThemeToggle from "@/components/ThemeToggle";
 
 export default function Header() {
+  const links = [
+    { href: "/", label: "Home" },
+    { href: "/projects", label: "Projects" },
+    // { href: "/blog", label: "Blog" },
+    { href: "/contact", label: "Contact" },
+  ];
+
   return (
     <header className="sticky top-0 z-20 bg-opacity-75 backdrop-blur-md py-6 border-b">
       <div className="container mx-auto max-w-3xl px-8">
         <div className="flex justify-between items-center gap-8">
           <div className="flex gap-8">
-            <Link
-              className="text-muted-foreground hover:text-primary title"
-              href="/"
-            >
-              Home
-            </Link>
-            <Link
-              className="text-muted-foreground hover:text-primary title"
-              href="/projects"
-            >
-              Projects
-            </Link>
-            {/* <Link
-              className="text-muted-foreground hover:text-primary title"
-              href="/blog"
-            >
-              Blog
-            </Link> */}
-            <Link
-              className="text-muted-foreground hover:text-primary title"
-              href="/contact"
-            >
-              Contact
-            </Link>
+            {links.map(({ href, label }) => (
+              <Link
+                key={href}
+                href={href}
+                className="text-muted-foreground hover:text-primary"
+              >
+                {label}
+              </Link>
+            ))}
           </div>
           <ThemeToggle />
         </div>
