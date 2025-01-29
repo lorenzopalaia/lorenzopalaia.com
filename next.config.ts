aebf5d4f6c06c59d90bf083703324a46131d59ad
lorenzopalaia <lorenzopalaia53@gmail.com>
2025-01-29 13:13:56 +0100

    Vercel tracking test

--- next.config.ts
+++ next.config.ts
@@ -15,28 +15,12 @@ const nextConfig: NextConfig = {
         source: "/(.*)",
         headers: [
           {
-            key: "Strict-Transport-Security",
-            value: "max-age=31536000; includeSubDomains; preload",
-          },
-          {
             key: "X-Content-Type-Options",
             value: "nosniff",
           },
           {
-            key: "X-Frame-Options",
-            value: "DENY",
-          },
-          {
-            key: "X-XSS-Protection",
-            value: "1; mode=block",
-          },
-          {
             key: "Referrer-Policy",
             value: "strict-origin-when-cross-origin",
-          },
-          {
-            key: "Permissions-Policy",
-            value: "camera=(), geolocation=(), payment=()",
           },
         ],
       },
