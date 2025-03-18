a3d6edf200d03f67e5c46c42c40afe7d41a55680
lorenzopalaia <lorenzopalaia53@gmail.com>
2025-03-18 13:00:27 +0100

    Minor

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+const config = {
   siteUrl: process.env.SITE_URL || "https://www.lorenzopalaia.com",
   generateRobotsTxt: true,
   // * use this to exclude routes from the sitemap (i.e. a user dashboard). By default, NextJS app router metadata files are excluded (https://nextjs.org/docs/app/api-reference/file-conventions/metadata)
@@ -18,4 +18,12 @@ module.exports = {
     "/_document",
     "/_error",
   ],
+  priority: {
+    "/": 1.0,
+    "/blog": 0.8,
+    "/projects": 0.8,
+    "/contact": 0.6,
+  },
 };
+
+export default config;
